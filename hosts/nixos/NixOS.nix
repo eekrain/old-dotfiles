e1a3ffda77e7c9502feaf1afe458b7b4f88a24ae
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-09 03:32:30 +0700

    basic xmonad on NixOS host

--- hosts/nixos/NixOS.nix
+++ hosts/nixos/NixOS.nix
@@ -1,12 +1,78 @@
-{ suites, ... }:
+{ suites, config, lib, pkgs, modulesPath, ... }:
 {
   ### root password is empty by default ###
   imports = suites.base;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Kernel
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
 
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  # Required, but will be overridden in the resulting installer ISO.
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/3543649e-326d-452d-8656-374b408c2d9a";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
   networking.networkmanager.enable = true;
 
-  fileSystems."/" = { device = "/dev/disk/by-label/nixos"; };
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  virtualisation.virtualbox.guest.enable = true;
+
+  # Display Settings
+  services = {
+    gnome.gnome-keyring.enable = true;
+    upower.enable = true;
+
+    dbus = {
+      enable = true;
+      packages = [ pkgs.dconf ];
+    };
+
+    xserver = {
+      enable = true;
+      layout = "us";
+
+      libinput = {
+        enable = true;
+        touchpad.disableWhileTyping = true;
+      };
+
+      displayManager.defaultSession = "none+xmonad";
+
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+      };
+
+      xkbOptions = "caps:ctrl_modifier";
+    };
+  };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  systemd.services.upower.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
 }
--- hosts/nixos/bootstrap.nix
+++ hosts/nixos/bootstrap.nix
@@ -10,5 +10,12 @@
   boot.loader.systemd-boot.enable = true;
 
   # Required, but will be overridden in the resulting installer ISO.
-  fileSystems."/" = { device = "/dev/disk/by-label/nixos"; };
+
+  # fileSystems."/" = { device = "/dev/disk/by-label/nixos"; };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/3543649e-326d-452d-8656-374b408c2d9a";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
 }
