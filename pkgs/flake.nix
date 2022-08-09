a021749b2b4e49006b5c2fe39b887d4e7089bdc6
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-09 17:06:40 +0700

    without nvfetcher

---
+++ pkgs/flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "Package sources";
+
+  inputs = {
+    sf-pro.url = "github:sahibjotsaggu/inih/San-Francisco-Pro-Fonts";
+    sf-pro.flake = false;
+  };
+}
--- pkgs/fonts/sf-pro/default.nix
+++ pkgs/fonts/sf-pro/default.nix
@@ -1,10 +1,12 @@
-{ lib, stdenv }:
-
-stdenv.mkDerivation {
+{ lib, stdenv, srcs, ... }:
+let inherit (srcs) sf-pro; in
+stdenv.mkDerivation
+{
   pname = "sf-pro";
 
   # version will resolve to the latest available on gitub
-  inherit (sources.sf-pro) version src;
+  inherit (sf-pro) version;
+  src = sf-pro;
 
   installPhase = ''
     mkdir -p $out/share/fonts/{truetype,opentype}
