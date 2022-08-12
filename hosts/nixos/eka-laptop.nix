fdffe88b97cf171e84d7c8656d4b04e7cbd35a60
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-13 01:50:15 +0700

    tes
    Former-commit-id: fe7717c62967ca5617e73b7998d8ac4d96843d5f [formerly 322b9727aa141337b4474ab7e994a871805d9517]
    Former-commit-id: 310382079854a190a602e1457ad982932f8b7128

--- hosts/nixos/eka-laptop.nix
+++ hosts/nixos/eka-laptop.nix
@@ -14,10 +14,120 @@
   };
 
   # Bootloader.
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    useOSProber = true;
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+
+    grub = {
+      enable = true;
+      version = 2;
+      device = "nodev";
+      efiSupport = true;
+      devices = [ "nodev" ];
+
+      useOSProber = false;
+
+
+      extraEntries = ''
+        menuentry "NixOS - Default" --class nixos --unrestricted {
+        search --set=drive1 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+        search --set=drive2 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+          linux ($drive2)/nix/store/2ybv1pydzh7jylm5lfdjc20m4yprkmw4-linux-5.15.56/bzImage init=/nix/store/w0cdc8zwgn1ymalrn3nka41kf2051ha0-nixos-system-nixos-22.05.2019.d9536b9b1e3/init loglevel=4
+          initrd ($drive2)/nix/store/sdq5ysh3kpzyj3cprxbzfwpzwxlcr109-initrd-linux-5.15.56/initrd
+        }
+
+
+        submenu "NixOS - All configurations" --class submenu {
+        menuentry "NixOS - Configuration 5 (2022-08-13 - 22.05.2019.d9536b9b1e3)" --class nixos {
+        search --set=drive1 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+        search --set=drive2 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+          linux ($drive2)/nix/store/2ybv1pydzh7jylm5lfdjc20m4yprkmw4-linux-5.15.56/bzImage init=/nix/store/w0cdc8zwgn1ymalrn3nka41kf2051ha0-nixos-system-nixos-22.05.2019.d9536b9b1e3/init loglevel=4
+          initrd ($drive2)/nix/store/sdq5ysh3kpzyj3cprxbzfwpzwxlcr109-initrd-linux-5.15.56/initrd
+        }
+
+        menuentry "NixOS - Configuration 4 (2022-08-12 - 22.05.2019.d9536b9b1e3)" --class nixos {
+        search --set=drive1 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+        search --set=drive2 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+          linux ($drive2)/nix/store/2ybv1pydzh7jylm5lfdjc20m4yprkmw4-linux-5.15.56/bzImage init=/nix/store/4qizd9fym1pa8949vbr3hgwwplsbl5qn-nixos-system-nixos-22.05.2019.d9536b9b1e3/init loglevel=4
+          initrd ($drive2)/nix/store/sdq5ysh3kpzyj3cprxbzfwpzwxlcr109-initrd-linux-5.15.56/initrd
+        }
+
+        menuentry "NixOS - Configuration 3 (2022-08-12 - 22.05.2019.d9536b9b1e3)" --class nixos {
+        search --set=drive1 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+        search --set=drive2 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+          linux ($drive2)/nix/store/2ybv1pydzh7jylm5lfdjc20m4yprkmw4-linux-5.15.56/bzImage init=/nix/store/amnnfvl2ndpb55n87w4m3ld1fnkyfmi8-nixos-system-nixos-22.05.2019.d9536b9b1e3/init loglevel=4
+          initrd ($drive2)/nix/store/sdq5ysh3kpzyj3cprxbzfwpzwxlcr109-initrd-linux-5.15.56/initrd
+        }
+
+        menuentry "NixOS - Configuration 2 (2022-08-12 - 22.05.2019.d9536b9b1e3)" --class nixos {
+        search --set=drive1 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+        search --set=drive2 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+          linux ($drive2)/nix/store/2ybv1pydzh7jylm5lfdjc20m4yprkmw4-linux-5.15.56/bzImage init=/nix/store/3xvl32a3i1irm8fsz958lclb7q22h5h2-nixos-system-nixos-22.05.2019.d9536b9b1e3/init loglevel=4
+          initrd ($drive2)/nix/store/sdq5ysh3kpzyj3cprxbzfwpzwxlcr109-initrd-linux-5.15.56/initrd
+        }
+
+        menuentry "NixOS - Configuration 1 (2022-08-12 - 22.05.2019.d9536b9b1e3)" --class nixos {
+        search --set=drive1 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+        search --set=drive2 --fs-uuid 74c9ef46-ccaa-4096-9ccb-670b9849b21b
+          linux ($drive2)/nix/store/2ybv1pydzh7jylm5lfdjc20m4yprkmw4-linux-5.15.56/bzImage init=/nix/store/wcw2l3qsrh87z550cnzcpwwiqkszjbm7-nixos-system-nixos-22.05.2019.d9536b9b1e3/init loglevel=4
+          initrd ($drive2)/nix/store/sdq5ysh3kpzyj3cprxbzfwpzwxlcr109-initrd-linux-5.15.56/initrd
+        }
+
+        }
+        menuentry 'Arch Linux (on /dev/sdb2)' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-simple-a737cad4-85d7-49af-bd66-72d8bcd986e6' {
+          insmod part_gpt
+          insmod fat
+          set root='hd1,gpt1'
+          if [ x$feature_platform_search_hint = xy ]; then
+            search --no-floppy --fs-uuid --set=root --hint-bios=hd1,gpt1 --hint-efi=hd1,gpt1 --hint-baremetal=ahci1,gpt1  E636-EFDE
+          else
+            search --no-floppy --fs-uuid --set=root E636-EFDE
+          fi
+          linux /vmlinuz-linux-zen root=UUID=a737cad4-85d7-49af-bd66-72d8bcd986e6 rw rootfstype=ext4 loglevel=3 quiet
+          initrd /intel-ucode.img
+        }
+        submenu 'Advanced options for Arch Linux (on /dev/sdb2)' $menuentry_id_option 'osprober-gnulinux-advanced-a737cad4-85d7-49af-bd66-72d8bcd986e6' {
+          menuentry 'Arch Linux (on /dev/sdb2)' --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-/vmlinuz-linux-zen--a737cad4-85d7-49af-bd66-72d8bcd986e6' {
+            insmod part_gpt
+            insmod fat
+            set root='hd1,gpt1'
+            if [ x$feature_platform_search_hint = xy ]; then
+              search --no-floppy --fs-uuid --set=root --hint-bios=hd1,gpt1 --hint-efi=hd1,gpt1 --hint-baremetal=ahci1,gpt1  E636-EFDE
+            else
+              search --no-floppy --fs-uuid --set=root E636-EFDE
+            fi
+            linux /vmlinuz-linux-zen root=UUID=a737cad4-85d7-49af-bd66-72d8bcd986e6 rw rootfstype=ext4 loglevel=3 quiet
+            initrd /intel-ucode.img
+          }
+          menuentry 'Arch Linux, with Linux linux-zen (on /dev/sdb2)' --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-/vmlinuz-linux-zen--a737cad4-85d7-49af-bd66-72d8bcd986e6' {
+            insmod part_gpt
+            insmod fat
+            set root='hd1,gpt1'
+            if [ x$feature_platform_search_hint = xy ]; then
+              search --no-floppy --fs-uuid --set=root --hint-bios=hd1,gpt1 --hint-efi=hd1,gpt1 --hint-baremetal=ahci1,gpt1  E636-EFDE
+            else
+              search --no-floppy --fs-uuid --set=root E636-EFDE
+            fi
+            linux /vmlinuz-linux-zen root=UUID=a737cad4-85d7-49af-bd66-72d8bcd986e6 rw rootfstype=ext4 loglevel=3 quiet
+            initrd /intel-ucode.img
+          }
+          menuentry 'Arch Linux, with Linux linux-zen (fallback initramfs) (on /dev/sdb2)' --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-/vmlinuz-linux-zen--a737cad4-85d7-49af-bd66-72d8bcd986e6' {
+            insmod part_gpt
+            insmod fat
+            set root='hd1,gpt1'
+            if [ x$feature_platform_search_hint = xy ]; then
+              search --no-floppy --fs-uuid --set=root --hint-bios=hd1,gpt1 --hint-efi=hd1,gpt1 --hint-baremetal=ahci1,gpt1  E636-EFDE
+            else
+              search --no-floppy --fs-uuid --set=root E636-EFDE
+            fi
+            linux /vmlinuz-linux-zen root=UUID=a737cad4-85d7-49af-bd66-72d8bcd986e6 rw rootfstype=ext4 loglevel=3 quiet
+            initrd /intel-ucode.img
+          }
+        }
+      '';
+    };
   };
 
   # File systems.
