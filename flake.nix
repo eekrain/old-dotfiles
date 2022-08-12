2aaf0e15661185fb5bb3dbc9a8225535363b40c3
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-12 00:28:57 +0000

    forgot adding awesome user profiles
    Former-commit-id: 9d1dd3f876a5d2cb25db09dc22d8e87af8dec6f5 [formerly 4bffcccf0ad703f23f75e2dfa6dcadc8597d5814]
    Former-commit-id: e1cd14991e4e8067554cd04c34c5553320428143

--- flake.nix
+++ flake.nix
@@ -165,7 +165,15 @@
           importables = rec {
             profiles = digga.lib.rakeLeaves ./users/profiles;
             suites = with profiles; rec {
-              base = [ direnv git basic-needs fish zsh neofetch ];
+              base = [
+                awesome
+                basic-needs
+                direnv
+                fish
+                git
+                neofetch
+                zsh
+              ];
             };
           };
           users = {
