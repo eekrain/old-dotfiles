c5fca6af5ae9159e9520a64a9386a39958f1b19b
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-19 22:37:03 +0700

    test
    Former-commit-id: c082e4d6af95e77eca3fe9139c1b0021ce9d9a38 [formerly 7676312b525f879f8724edece745ec6b9d6ff37d]
    Former-commit-id: 777d70c5e864503d8da60fbbf985ebd2a5df7d96

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # set ssh public keys here for your system and user
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJOfjODny9hbeBTqh00cqSjkdoyEu6mWXlEfSdu93EI root@eka-laptop";
-  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAG2IwZWpQgdHs0Fr8Jl2SlZZ3+gGll4pLtozl4oAez/ eekrain@eka-laptop";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJOfjODny9hbeBTqh00cqSjkdoyEu6mWXlEfSdu93EI";
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAG2IwZWpQgdHs0Fr8Jl2SlZZ3+gGll4pLtozl4oAez/";
   allKeys = [ system user ];
 in
 {
