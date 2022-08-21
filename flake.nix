31b6487b77ffe0ce4e30b6f6c59a6b9b8b4370aa
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-21 18:25:44 +0700

    test
    Former-commit-id: 78cfbea97ca6de453d824b43a2e1324ea05fb05c [formerly 924928748447abd125fe91f20f3e24324707c1cd]
    Former-commit-id: 3591df54b164354003c77d2774c81b01f5cdaf3e

--- flake.nix
+++ flake.nix
@@ -118,6 +118,29 @@
               digga.nixosModules.nixConfig
               home.nixosModules.home-manager
               agenix.nixosModules.age
+              {
+                home-manager.users.eekrain = { config, lib, pkgs, ... }: {
+                  imports = [
+                    android-nixpkgs.hmModule
+
+                    {
+                      inherit config lib pkgs;
+                      android-sdk.enable = true;
+
+                      # Optional; default path is "~/.local/share/android".
+                      android-sdk.path = "${config.home.homeDirectory}/.android/sdk";
+
+                      android-sdk.packages = sdk: with sdk; [
+                        build-tools-31-0-0
+                        cmdline-tools-latest
+                        emulator
+                        platforms-android-31
+                        sources-android-31
+                      ];
+                    }
+                  ];
+                };
+              }
             ];
           };
 
--- overlays/android-nixpkgs.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, inputs, ... }:
-{
-  # android-nixpkgs overlay
-  nixpkgs.overlays = [
-    inputs.android-nixpkgs.overlay
-  ];
-}
