15e7aa3411fd051a95705d1d766ad342e036850d
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-21 18:32:44 +0700

    test
    Former-commit-id: 8ec5c2dca637517a0b687b6f3c7ab39b5fb71a52 [formerly 2d50859686fdab0c10bcbe68dbcc5755a97db2ba]
    Former-commit-id: 50f6270d0b4973ece71eb8fab06bdea9a7845011

--- flake.nix
+++ flake.nix
@@ -118,29 +118,6 @@
               digga.nixosModules.nixConfig
               home.nixosModules.home-manager
               agenix.nixosModules.age
-              {
-                home-manager.users.eekrain = { config, lib, pkgs, ... }: {
-                  imports = [
-                    android-nixpkgs.hmModule
-
-                    {
-                      inherit config lib pkgs;
-                      android-sdk.enable = true;
-
-                      # Optional; default path is "~/.local/share/android".
-                      android-sdk.path = "${config.home.homeDirectory}/.android/sdk";
-
-                      android-sdk.packages = sdk: with sdk; [
-                        build-tools-31-0-0
-                        cmdline-tools-latest
-                        emulator
-                        platforms-android-31
-                        sources-android-31
-                      ];
-                    }
-                  ];
-                };
-              }
             ];
           };
 
@@ -155,7 +132,7 @@
               users = digga.lib.rakeLeaves ./users;
             };
             suites = with profiles; rec {
-              base = [ graphical.awesome core.nixos users.root users.eekrain ];
+              base = [ graphical.awesome core.nixos users.root users.eekrain android ];
             };
           };
         };
---
+++ profiles/android/default.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, android-nixpkgs, ... }:
+
+let
+  android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+    cmdline-tools-latest
+    build-tools-32-0-0
+    platform-tools
+    platforms-android-31
+    emulator
+  ]);
+in
+{
+  environment.systemPackages = with pkgs; [
+    # IDE
+    android-tools
+    android-sdk
+    android-studio
+    flutter
+  ];
+  environment.variables = {
+    ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
+    ANDROID_HOME = "$ANDROID_SDK_ROOT";
+  };
+}
