b1a835f2b8968a80f473386fa36d576c7ac33f31
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-29 22:47:03 +0700

    picom start by bspwmrc
    Former-commit-id: b5e78027570c919bd948871b28331c1f1a932a23 [formerly f8bb6218deac71b49823f0685825b0f398209630]
    Former-commit-id: 484c01eb3a6a6153b8fdd60d6d2684d3104917e1

--- users/profiles/bspwm/picom/default.nix
+++ users/profiles/bspwm/picom/default.nix
@@ -1,56 +1,6 @@
-{ config, pkgs, ... }:
+{ pkgs, config, ... }:
 {
-  services.picom = {
-    enable = true;
+  home.packages = with pkgs; [ picom ];
 
-    fade = true;
-    fadeDelta = 10;
-    fadeSteps = [ 0.06 0.06 ];
-
-    shadow = true;
-    shadowOffsets = [ -20 -20 ];
-    shadowOpacity = 0.55;
-
-    shadowExclude = [
-      "_GTK_FRAME_EXTENTS@:c"
-      "class_g = 'firefox' && argb"
-      "class_g ?= 'eww-calendar'"
-      "class_g ?= 'eww-control-center'"
-      "class_g ?= 'eww-info-center'"
-      "class_g ?= 'eww-lock-screen'"
-      "class_g ?= 'eww-notification-center'"
-      "class_g ?= 'eww-notification-popup'"
-    ];
-
-    wintypes = {
-      dialog = {
-        shadow = false;
-      };
-      dnd = {
-        shadow = false;
-      };
-      dropdown_menu = {
-        shadow = false;
-      };
-      menu = {
-        shadow = false;
-        full-shadow = true;
-      };
-      popup_menu = {
-        shadow = false;
-        full-shadow = true;
-      };
-      tooltip = {
-        fade = true;
-        shadow = true;
-        full-shadow = false;
-      };
-      utility = {
-        shadow = false;
-      };
-    };
-
-    backend = "glx";
-    vSync = true;
-  };
+  xdg.configFile."picom".source = ./picom;
 }
