930b3e2096bdd1fb6b848a7025ee8bf303c2704f
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-29 22:49:29 +0700

    update picom
    Former-commit-id: 6e91e2960d24bacc5b6d23b7c47ec52fa794089c [formerly c114a627466789ce701945e50893a4d7e697ca1b]
    Former-commit-id: c8387a4ce3cffea1569e86e9f448bb93060f9faa

--- users/profiles/bspwm/default.nix
+++ users/profiles/bspwm/default.nix
@@ -8,7 +8,6 @@
 
   xdg.configFile."bspwm".source = ./bspwm-config;
   xdg.configFile."bspwm".recursive = true;
-  # xdg.configFile."picom".source = ./picom;
   programs = {
     eww = {
       enable = true;
