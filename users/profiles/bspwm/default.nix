f31fda69d80e1b046aa1d2f529b247d2247fc527
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-29 23:38:58 +0700

    add rofi
    Former-commit-id: 7d175acd921687f7c304e28e5b70af515520d303 [formerly 36756de2c9407d197d15ac8568100e7e160657d2]
    Former-commit-id: fea646db61ae7841b9b39eb4acec4bedc8359672

--- users/profiles/bspwm/default.nix
+++ users/profiles/bspwm/default.nix
@@ -8,6 +8,11 @@
 
   xdg.configFile."bspwm".source = ./bspwm-config;
   xdg.configFile."bspwm".recursive = true;
+  xdg.configFile."rofi".source = ./rofi;
+  xdg.configFile."rofi".recursive = true;
+  xdg.configFile."bin".source = ./bin;
+  xdg.configFile."bin".recursive = true;
+
   programs = {
     eww = {
       enable = true;
