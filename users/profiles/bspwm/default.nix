ba13a90b7a062181d26223215af38cc8a67613a9
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-29 22:02:53 +0700

    add picom.conf
    Former-commit-id: ed4f4f00bb67cc3a898849440c196af37ad98df3 [formerly d8177921e16a5ea673884ecb7dd199cf32b04564]
    Former-commit-id: 4a7db0f6470c81d029a653f156a5fa0791e48f4c

--- users/profiles/bspwm/default.nix
+++ users/profiles/bspwm/default.nix
@@ -7,6 +7,7 @@
 
   xdg.configFile."bspwm".source = ./bspwm-config;
   xdg.configFile."bspwm".recursive = true;
+  xdg.configFile."picom".source = ./picom;
   programs = {
     eww = {
       enable = true;
