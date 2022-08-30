f3bebaff9d06f9c1c992eb9aa1138d17a95c0ef6
Ardian Eka Candra <ardianoption@gmail.com>
2022-08-30 17:17:38 +0700

    should be on directory
    Former-commit-id: 67daaa988f03ac8f44761a2d29cf8339175b8550 [formerly fc5b87113fdd8d7dc2a11e63747594ce7c1658c8]
    Former-commit-id: c4ddd1adbda22f8d14c39e9716d77dadb83c21f9

--- pkgs/chwb2/default.nix
+++ pkgs/chwb2/default.nix
@@ -4,7 +4,8 @@
 }:
 stdenv.mkDerivation {
   name = "chwb2";
-  src = ./chwb2; # Get the ... part with nix-prefetch-github
+  pname = "chwb2";
+  src = ./src; # Get the ... part with nix-prefetch-github
   phases = [ "installPhase" ];
   installPhase = "mkdir -p $out/bin; cp -r $src $out/bin";
 }
