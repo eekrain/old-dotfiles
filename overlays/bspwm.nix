8e22a6a82eeaea8e54c0d1fb176b5c9ba7a235df
Ardian Eka Candra <ardianoption@gmail.com>
2022-09-01 03:17:59 +0700

    use phuhl version of bspwm-rounded
    Former-commit-id: 001035a71d0edac62c76c775d73f6bc91c194650 [formerly 36f707cd42604181370522b52d26b8b03f715147]
    Former-commit-id: d1ef1cb9c17af04eafeebeb2831fbf9621365215

--- overlays/bspwm.nix
+++ overlays/bspwm.nix
@@ -1,5 +1,5 @@
 final: prev: {
   bspwm = prev.bspwm.overrideAttrs (o: rec{
-    inherit (prev.sources.bspwm-rounded-corners) version src;
+    inherit (prev.sources.bspwm-rounded) version src;
   });
 }
