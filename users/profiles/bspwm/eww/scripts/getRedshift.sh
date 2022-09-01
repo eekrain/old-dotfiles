f7aed65ec8f69289cbf3cdf4b05424047dfb2da9
Ardian Eka Candra <ardianoption@gmail.com>
2022-09-02 04:59:57 +0700

    kill redshift on toggle
    Former-commit-id: 57ed6308772c27b516f33cf17c89942544526825 [formerly 2811f054233c7a3d5b2f9a63de5b7bd56108d470]
    Former-commit-id: 2374bcdb7079d35f5d9740b7751ce16249b1d2e3

--- users/profiles/bspwm/eww/scripts/getRedshift.sh
+++ users/profiles/bspwm/eww/scripts/getRedshift.sh
@@ -3,16 +3,17 @@
 LOCK_FILE="$HOME/.cache/eww-redshift.lock"
 
 case $1 in
-	"color")
-		[[ ! -f "$LOCK_FILE" ]] && echo "$bgSecondary" || echo "#1c2325"
-		;;
-	"toggle")
-		if [[ ! -f "$LOCK_FILE" ]]; then
-			touch "$LOCK_FILE"
-			redshift -P -l -7.8321757340401605:110.35956370306734
-		else
-			rm "$LOCK_FILE"
-			redshift -x
-		fi
-		;;
+  "color")
+    [[ ! -f "$LOCK_FILE" ]] && echo "$bgSecondary" || echo "#1c2325"
+  ;;
+  "toggle")
+    if [[ ! -f "$LOCK_FILE" ]]; then
+      touch "$LOCK_FILE"
+      redshift -P -l -7.8321757340401605:110.35956370306734
+    else
+      rm "$LOCK_FILE"
+      redshift -x
+      pkill redshift
+    fi
+  ;;
 esac
